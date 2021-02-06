0ef1e4e3ddc916c36f676f380ff4ed0a4c784526
frogwz <frogwz@126.com>
2021-02-06 22:13:21 +0800

    111

---
+++ src/components/Page/index.js
@@ -0,0 +1,23 @@
+// @flow strict
+export { default } from './Page';
+export const pageQuery = graphql`
+query {
+allPosts(
+filter: { status: { eq: "publish" } }
+sort: { fields: publish_date___startDate, order: DESC }
+) {
+totalCount
+edges {
+node {
+id
+tags
+title
+slug
+publish_date {
+startDate(formatString: "YYYY-MM-DD")
+}
+}
+}
+}
+}
+`
