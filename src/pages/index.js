648a8a74c569409ca13dcbcd04756c73c0c2b400
frogwz <frogwz@126.com>
2021-02-06 19:23:30 +0800

    11

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <a href='./speed/example-multipleServers-full.html'>multipleServersfull</a>
+    <a href='./speed/example-multipleServers-pretty.html'>multipleServerspretty</a>
+    <a href='./speed/example-singleServer-basic.html'>singleServerbasic</a>
+    <a href='./speed/example-singleServer-chart.html'>singleServerchart</a>
+    <a href='./speed/example-singleServer-customSettings.html'>singleServercustomSettings</a>
+    <a href='./speed/example-singleServer-full.html'>singleServerfull</a>
+    <a href='./speed/example-singleServer-gauges.html'>singleServergauges</a>
+    <a href='./speed/example-singleServer-pretty.html'>singleServerpretty</a>
+    <a href='./speed/example-singleServer-progressBar.html'>singleServerprogressBar</a>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default IndexPage
